020160b5d71d6cc5e01f21bd0f0827f7167ca3fe
Tom Croft <tom.croft@rightster.com>
2015-07-13 01:29:15 +0100

    Sorting tested * Added Sort\SortCollection * Added inheritance for Sort objects * Sort 100% tested * Code coverage ~40.73%

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,16 @@ namespace tantrum_elastic\tests;
 
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
+
+    public function invalidStringsDataProvider()
+    {
+        return [
+            [1],
+            [1.1],
+            [[]],
+            [new \stdClass()],
+            [true],
+            [false],
+        ];
+    }
 }
