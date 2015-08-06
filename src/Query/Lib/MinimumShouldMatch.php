6da73361f6406be1086bb58296547a275145c687
Tom Croft <tom@desiato.io>
2015-08-06 21:47:59 +0100

    Implemented MultiMatch Query * Resolves #9 * Merge https://github.com/tomcroft/tantrum_elastic/pull/78 first

--- src/Query/Lib/MinimumShouldMatch.php
+++ src/Query/Lib/MinimumShouldMatch.php
@@ -7,10 +7,22 @@ trait MinimumShouldMatch
     /**
      * @param integer $minimumShouldMatch
      */
-    public function setMinimumMustMatch($minimumShouldMatch)
+    public function setMinimumShouldMatch($minimumShouldMatch)
     {
         $this->validateMinimumInteger($minimumShouldMatch, 1, 'minimum _should_match must be a positive integer');
         $this->addOption('minimum_should_match', $minimumShouldMatch);
         return $this;
     }
+
+    /**
+     * Ensures that classes using this trait also use the Lib\Validate\Integers trait
+     * @param $value
+     */
+    abstract protected function validateMinimumInteger($value, $maxValue, $message = null, $exceptionClass = null);
+
+    /**
+     * Ensures that classes using this trait extend Lib\Element
+     * @param $value
+     */
+    abstract protected function addOption($key, $value);
 }
